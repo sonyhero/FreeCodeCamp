72898df2c01100ef1eb3ffcdf394abde163399f8
sonyhero <27anton06@gmail.com>
2022-12-24 22:39:10 +0300

    Задание 92

---
+++ 92 manipulating-complex-objects.js
@@ -0,0 +1,24 @@
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+    },
+  {
+    "artist": "Linkin Park",
+    "title": "Meteora",
+    "release_year": 2003,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+    }
+];
