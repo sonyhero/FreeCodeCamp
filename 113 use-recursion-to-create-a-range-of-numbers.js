ad86815a4b34ca3aa8f5531dc6124d5da2671382
sonyhero <27anton06@gmail.com>
2022-12-25 22:32:48 +0300

    Задание 113

---
+++ 113 use-recursion-to-create-a-range-of-numbers.js
@@ -0,0 +1,10 @@
+function rangeOfNumbers(startNum, endNum) {
+  if (startNum == endNum) {
+  return [startNum];
+  } else {
+    const arr = rangeOfNumbers(startNum, endNum - 1);
+    arr.push(endNum);
+    return arr;
+  }
+
+};
