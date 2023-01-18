0e549cb350d7431e5db28161321e2af4d7975084
sonyhero notebook <27anton06@gmail.com>
2023-01-18 12:17:26 +0300

    format 15

--- JavaScript Algorithms and Data Structures/ES6/15 create-strings-using-template-literals.js
+++ JavaScript Algorithms and Data Structures/ES6/15 create-strings-using-template-literals.js
@@ -1,15 +1,15 @@
 const result = {
   success: ["max-length", "no-amd", "prefer-arrow-functions"],
   failure: ["no-var", "var-on-top", "linebreak"],
-  skipped: ["no-extra-semi", "no-dup-keys"]
+  skipped: ["no-extra-semi", "no-dup-keys"],
 };
 function makeList(arr) {
   // Only change code below this line
   const failureItems = [];
-  for ( let i = 0; i < arr.length; i++ ) {
-failureItems.push(`<li class="text-warning">${arr[i]}</li>`)
+  for (let i = 0; i < arr.length; i++) {
+    failureItems.push(`<li class="text-warning">${arr[i]}</li>`);
   }
-  
+
   // Only change code above this line
 
   return failureItems;
