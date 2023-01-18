e9cf7cf132e74781fcd609f0523775f998255b01
sonyhero notebook <27anton06@gmail.com>
2023-01-18 12:17:09 +0300

    format 14

--- JavaScript Algorithms and Data Structures/ES6/14 use-destructuring-assignment-to-pass-an-object-as-a-functions-parameters.js
+++ JavaScript Algorithms and Data Structures/ES6/14 use-destructuring-assignment-to-pass-an-object-as-a-functions-parameters.js
@@ -4,9 +4,9 @@ const stats = {
   median: 34.54,
   mode: 23.87,
   min: -0.75,
-  average: 35.85
+  average: 35.85,
 };
 
 // Only change code below this line
-const half = ({max, min}) => (max + min) / 2.0; 
+const half = ({ max, min }) => (max + min) / 2.0;
 // Only change code above this line
