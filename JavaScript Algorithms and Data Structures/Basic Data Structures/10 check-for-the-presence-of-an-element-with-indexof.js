a868e22ea34571553e2416d6ccb71a0bf814698c
sonyhero <27anton06@gmail.com>
2023-01-29 19:09:55 +0300

    delete some files

--- JavaScript Algorithms and Data Structures/Basic Data Structures/10 check-for-the-presence-of-an-element-with-indexof.js
+++
@@ -1,9 +0,0 @@
-function quickCheck(arr, elem) {
-  // Only change code below this line
-  if (arr.indexOf(elem) == -1) {
-    return false;
-  } else return true;
-  // Only change code above this line
-}
-
-console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
