a5e4fbdd1401c8a60d4c4446397bcb014383ee50
sonyhero notebook <27anton06@gmail.com>
2023-01-31 13:24:00 +0300

    add 10 implement-the-filter-method-on-a-prototype

--- JavaScript Algorithms and Data Structures/Functional Programming/08 implement-map-on-a-prototype.js
+++ JavaScript Algorithms and Data Structures/Functional Programming/08 implement-map-on-a-prototype.js
@@ -1,7 +1,11 @@
 Array.prototype.myMap = function(callback) {
   const newArray = [];
   // Only change code below this line
-this.forEach( (a,b,c) => newArray.push(callback(a,b,c)))
+this.forEach( (a,b,c) => {
+  if(callback(a,b,c)) {
+    newArray.push(a)
+  }
+} )
   // Only change code above this line
   return newArray;
 };
