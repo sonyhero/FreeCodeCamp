224b3ced7b04bc79fe782d853dee511184763178
sonyhero notebook <27anton06@gmail.com>
2023-02-01 21:26:59 +0300

    delete some files

--- JavaScript Algorithms and Data Structures/Basic Algorithm Scripting/16 chunky-monkey.js
+++
@@ -1,9 +0,0 @@
-function chunkArrayInGroups(arr, size) {
-  let tmp = [];
-  for (let i = 0; i < arr.length; i += size) {
-    tmp.push(arr.slice(i, i + size));
-  }
-  return tmp;
-}
-
-chunkArrayInGroups(["a", "b", "c", "d"], 2);
