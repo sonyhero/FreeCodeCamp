a5426b309bc7c983d62c3be572b57dfa439c96de
sonyhero notebook <27anton06@gmail.com>
2023-02-01 21:16:25 +0300

    delete some files

--- JavaScript Algorithms and Data Structures/Basic Algorithm Scripting/11 title-case-a-sentence.js
+++
@@ -1,9 +0,0 @@
-function titleCase(str) {
-  return  str.split(" ")
-          .map((word) => {
-            return word[0].toUpperCase() + word.toLowerCase().slice(1);
-          })
-          .join(" ");
-}
-
-titleCase("I'm a little tea pot");
