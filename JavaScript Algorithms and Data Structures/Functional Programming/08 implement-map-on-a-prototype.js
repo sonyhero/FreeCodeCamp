cb06ca0f5a15707e4550d88c9e050dc3c6cf3bb9
sonyhero <27anton06@gmail.com>
2023-02-03 22:47:02 +0300

    del 08.js

--- JavaScript Algorithms and Data Structures/Functional Programming/08 implement-map-on-a-prototype.js
+++
@@ -1,7 +0,0 @@
-Array.prototype.myMap = function(callback) {
-  const newArray = [];
-  // Only change code below this line
-this.forEach( (a,b,c) =>  newArray.push(callback(a,b,c)))
-  // Only change code above this line
-  return newArray;
-};
