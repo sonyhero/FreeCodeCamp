73f99fc782936621d3113ab03eced9da79b107d0
sonyhero <27anton06@gmail.com>
2023-02-06 21:23:09 +0300

    del 21.js

--- JavaScript Algorithms and Data Structures/Functional Programming/21 apply-functional-programming-to-convert-strings-to-url-slugs.js
+++
@@ -1,6 +0,0 @@
-// Only change code below this line
-function urlSlug(title) {
-  return title.toLowerCase().split(' ').filter(x=> x !== "").join('-')
-}
-// Only change code above this line
-urlSlug("A Mind Needs Books Like A Sword Needs A Whetstone");
