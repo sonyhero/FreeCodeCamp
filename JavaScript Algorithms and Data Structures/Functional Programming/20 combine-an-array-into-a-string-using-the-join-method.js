b1fdddf863832f3e8ad6224bb800a2be2c3d188b
sonyhero <27anton06@gmail.com>
2023-02-06 21:22:20 +0300

    del 20.js

--- JavaScript Algorithms and Data Structures/Functional Programming/20 combine-an-array-into-a-string-using-the-join-method.js
+++
@@ -1,8 +0,0 @@
-function sentensify(str) {
-  // Only change code below this line
-return str.split(/[^a-z]/gi).join(' ');
-
-  // Only change code above this line
-}
-
-sentensify("May-the-force-be-with-you");
