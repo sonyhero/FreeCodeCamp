7249ea0df8bd152db9033b141a7d1642cc57a47e
sonyhero <27anton06@gmail.com>
2023-02-07 15:00:16 +0300

    07 - 1 part

--- JavaScript Algorithms and Data Structures/Intermediate Algorithm Scripting/07 search-and-replace.js
+++ JavaScript Algorithms and Data Structures/Intermediate Algorithm Scripting/07 search-and-replace.js
@@ -4,3 +4,16 @@
 Третий аргумент - то, чем вы будете заменять второй аргумент (после).
 Примечание: При замене сохраняйте регистр первого символа исходного слова. 
 Например, если вы хотите заменить слово Book на слово dog, оно должно быть заменено как Dog*/
+
+//("He is Sleeping on the couch", "Sleeping", "sitting") --> "He is Sitting on the couch"
+let str = "He is Sleeping on the couch";
+let before = "Sleeping";
+let after = "sitting";
+//console.log(after[0].toUpperCase() + after.slice(1, after.length))
+
+function myReplace(str, before, after) {
+    if (before[0].toUpperCase() == before[0] ) {
+        return after[0].toUpperCase() + after.slice(1, after.length)
+    } else return after[0].toLowerCase() + after.slice(1, after.length)
+}
+console.log(myReplace (str, before, after))
