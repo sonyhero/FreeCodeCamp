d36d7ca5dac3369e74e8b8217f9f40d3a33fa643
sonyhero <27anton06@gmail.com>
2023-02-07 14:38:07 +0300

    done!

--- JavaScript Algorithms and Data Structures/Intermediate Algorithm Scripting/06 pig-latin.js
+++ JavaScript Algorithms and Data Structures/Intermediate Algorithm Scripting/06 pig-latin.js
@@ -1,6 +1,24 @@
-function translatePigLatin(str) {
-  return str;
+//let str = "algorithm" //algorithmway
+let str = "schwartz" //artzschway
+
+/*function translatePigLatin(str) {
+  let vowels = "aeiouy";
+  for (let i = 0; i < vowels.length; i++) {
+    if (str[0] == vowels[i]) {
+      return str + 'way';
+    }
+  }
+}*/
+
+function translatePigLatin(str, charPos = 0) {
+  return ['a', 'e', 'i', 'o', 'u'].includes(str[0])
+    ? str + (charPos === 0 ? 'way' : 'ay')
+    : charPos === str.length
+      ? str + 'ay'
+      : translatePigLatin(str.slice(1) + str[0], charPos + 1);
 }
+
+console.log(translatePigLatin(str))
 //преобразовать строку в массив
 //сделать проверку первых букв - глассная или нет через метод includes???
 //далее по условию задачи
