ebaa748c0eee7ebea4a61724d5d5ea2590180b47
sonyhero <27anton06@gmail.com>
2023-02-10 17:35:40 +0300

    03 seek-and-destroy

---
+++ JavaScript Algorithms and Data Structures/Intermediate Algorithm Scripting/03 seek-and-destroy.js
@@ -0,0 +1,20 @@
+/*
+Seek and Destroy
+You will be provided with an initial array
+(the first argument in the destroyer function), 
+followed by one or more arguments. 
+Remove all elements from the initial array that are of the same value as these arguments.
+
+Note: You have to use the arguments object.
+
+destroyer([1, 2, 3, 1, 2, 3], 2, 3) --> [1,1]
+*/
+
+
+
+function destroyer(arr,...arg) {
+  return arr
+  
+}
+
+destroyer([1, 2, 3, 1, 2, 3], 2, 3);
