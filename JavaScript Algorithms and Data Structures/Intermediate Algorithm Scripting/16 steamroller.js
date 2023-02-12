548dd2cc88e7abdd1529e646422a9eb79bc6e254
sonyhero notebook <27anton06@gmail.com>
2023-02-12 19:13:03 +0300

    16 steamroller - через рекурсию

--- JavaScript Algorithms and Data Structures/Intermediate Algorithm Scripting/16 steamroller.js
+++ JavaScript Algorithms and Data Structures/Intermediate Algorithm Scripting/16 steamroller.js
@@ -3,9 +3,22 @@ Steamroller
 Flatten a nested array. You must account for varying levels of nesting.
 */
 
+// испоьзование flat()
 function steamrollArray(arr) {
   return arr.flat(Infinity);
 }
 
+//через reduce() и рекурсию
+function steamrollArray(arr) {
+  return Infinity > 0
+    ? arr.reduce(
+        (acc, val) =>
+          acc.concat(
+            Array.isArray(val) ? steamrollArray(val, Infinity - 1) : val
+          ),
+        []
+      )
+    : arr.slice();
+}
 
 console.log(steamrollArray([1, [2], [3, [[4]]]]));
